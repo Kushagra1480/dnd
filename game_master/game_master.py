aa22ef5ec40e481d23eb006bfd294048dcb471b1
KirtanSoni <kgsoni@asu.edu>
2024-07-26 15:20:37 +0530

    working multiplayer GAME Master

--- game_master/game_master.py
+++ game_master/game_master.py
@@ -28,15 +28,14 @@ class Conversation(BaseModel):
     messages: List[Message]
 
 
-@app.post("/play/{room_id}/init")
+@app.post("/play/{room_id}/generateCharacters")
 def initialize(room_id:str):
-    logger.info(f"Initializing Game for Room ID : {room_id}")
-    # load doc
-    try:
-        pass #load doc from database
-    except Exception as e:
-        logger.error(e)
-
+    logger.info(f"Generating Characters for Room ID : {room_id}")
+    # load doc TODO
+    _,context,_ = get_room_config(room_id=room_id)
+    prompt = initialize_prompt_template.format(context=context)
+    result = chat.invoke(input=[prompt])
+    return {"room_id": room_id, "Game_Master": result}
 
 
 @app.post("/play/{room_id}")
@@ -44,7 +43,7 @@ def response(room_id:str,conversation:Conversation):
     logger.info(f"Recieved conversation from Room ID : {room_id}")
     theme, context, mood_setting = get_room_config(room_id=room_id)
     curr_prompt = system_message_prompt.format(theme= theme, context=context, mood_setting=mood_setting)
-    messages = [curr_prompt] + generate_conversation_template(conversation=conversation.messages)
+    messages = [curr_prompt] + [player_context_prompt(room_id=room_id)] + generate_conversation_template(conversation=conversation.messages)
     result = chat.invoke(input=messages)
     return {"room_id": room_id, "Game_Master": result}
 
@@ -61,7 +60,6 @@ TEST_DATA = {
 
 def get_room_config(room_id:str)->list:
     # fetch from database TODO
-    
     return [TEST_DATA["Theme"],TEST_DATA["context"],TEST_DATA["mood_setting"]]
 
 
@@ -101,3 +99,62 @@ def generate_conversation_template(conversation):
     for message in conversation:
         chain.append(ROLE_MSG_TEMPLATE_MAP[message.role](content = message.content))
     return chain
+
+
+
+
+## GAME ------
+
+
+# Initialize Character DATA
+character_prompt_template = """ generate a list of 5 races and classes of players based on the given plot: {context}.
+    output format json 
+    races: [(race : name, description: desc),.........]
+    class: [(class : name, description: desc),.........]
+    """
+character_prompt = PromptTemplate(template= character_prompt_template, input_variables= ["context"])
+initialize_prompt_template = SystemMessagePromptTemplate(prompt=character_prompt)
+
+Player_Example = {
+    "name":"ifliit",
+    "class": "Warrior",
+    "race": "elf",   
+    "backstory":""
+}
+
+
+
+
+
+
+# Load Character Data as prompt
+
+load_character_prompt_template = """\n\nPlayers Playing this Game : {character_data}"""
+load_character_prompt = PromptTemplate(template=load_character_prompt_template, input_variables= ["character_data"])
+system_load_character_prompt = SystemMessagePromptTemplate(prompt=load_character_prompt)
+
+
+DUMMY_CHARACTER_DATA = """
+    Player 1
+    Name: draco
+    class:Ranger (Skilled in wilderness survival and tracking, rangers excel at exploring and navigating through the magic forest.)
+    race: elf (Graceful and mystical beings with a deep connection to nature, perfect for exploring the magic forest.)
+    backstory: child prodigy, high magic capacity 
+    
+
+    Player 2
+    Name: nomu
+    class: Bard (Charismatic performers and storytellers, bards can use their talents to charm and entertain the magical creatures of the forest during their exploration)
+    race: Gnome (Small and clever creatures with a knack for tinkering and a natural curiosity that drives them to explore the mysteries of the magic forest.)
+    backstory: poor family, steals from rich gives to poor
+"""
+
+def player_context_prompt(room_id):
+    #load data from db TODO
+    character_data = DUMMY_CHARACTER_DATA # this will be updated by the front end. 
+    return system_load_character_prompt.format(character_data=character_data)
+
+
+
+
+## voice tts
--- game_master/test.py
+++ game_master/test.py
@@ -1,17 +1,30 @@
 import requests
+import json
 url = "http://127.0.0.1:8000/play/123"
+def play_game_test():
+    convo =[]
+    while True:
+        inputstr = input ("\n : ")
+        message = { "role": "human",
+                    "content": inputstr}
+        convo.append(message)
+        converstaion = {"messages":convo}
+        response = requests.post(url=url,json=converstaion)
+        ai = response.json()["Game_Master"]["content"]
+        print("\n\nGame Master: " + ai)
+        ai_message = {"role":"assisant",
+                    "content": ai}
+        convo.append(ai_message)
 
-convo =[]
-while True:
-    inputstr = input ("\n : ")
-    message = { "role": "human",
-                "content": inputstr}
-    convo.append(message)
-    converstaion = {"messages":convo}
-    response = requests.post(url=url,json=converstaion)
-    ai = response.json()["Game_Master"]["content"]
-    print(ai)
-    ai_message = {"role":"assisant",
-                  "content": ai}
-    convo.append(ai_message)
 
+
+character_selection_url = "http://127.0.0.1:8000/play/123/generateCharacters"
+def generate_character_test():
+    response = requests.post(url=character_selection_url)
+    game_master = response.json()["Game_Master"]["content"]# type str
+    game_master = json.loads(game_master)
+    print(game_master)
+    with open('result.json', 'w') as fp:
+        json.dump(game_master, fp)
+
+play_game_test()
